8bf641d64350d6974ce2dccc8d79ad67a2b1556a
Roman Joseph Gallardo <romanjosephgallardo01@gmail.com>
2025-07-08 10:52:37 +0800

    Conditional Rendering test

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,13 +1,9 @@
-import Student from './Student.jsx'
+import UserGreeting from './UserGreeting.jsx'
 
 function App() {
   return (
     <>
-      <Student name="Spongebob" age={30} isStudent={true}/>
-      <Student name="Patrick" age={31} isStudent={true}/>
-      <Student name="Squidward" age={50} isStudent={false}/>
-      <Student name="Sandy" age={39} isStudent={false}/>
-      <Student />
+      <UserGreeting isLoggedIn={true} username="Jace" />
     </>
   );
 }
